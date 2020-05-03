069a06069a3b21d519c8b04f9b83682b48405cde
Jenny Arenas <jennymarin1989@hotmail.com>
2020-05-03 16:12:20 +0100

    Updates

---
+++ precache-manifest.6d901a2cae5ed3f330902424bc54bf6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9dee49b4326fcc3fe7ade7efe078ad",
+    "url": "/mothers-day/index.html"
+  },
+  {
+    "revision": "d3f6f741d3f1d7cb0ea0",
+    "url": "/mothers-day/static/css/main.b38faa5c.chunk.css"
+  },
+  {
+    "revision": "f9f18dfba399b7295763",
+    "url": "/mothers-day/static/js/2.704b34b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mothers-day/static/js/2.704b34b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3f6f741d3f1d7cb0ea0",
+    "url": "/mothers-day/static/js/main.55cb805f.chunk.js"
+  },
+  {
+    "revision": "81a914d270af55b7b35b",
+    "url": "/mothers-day/static/js/runtime-main.a7fbda5d.js"
+  },
+  {
+    "revision": "c433128143804b9c070bb64ef799ad8e",
+    "url": "/mothers-day/static/media/HMD3.c4331281.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mothers-day/static/media/logo.5d5d9eef.svg"
+  }
+]);
